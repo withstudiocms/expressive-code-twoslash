4a250393fc6caf1bc662febeab38e6599e8a2ea5
Adam Matthiesen <amatthiesen@outlook.com>
2026-02-20 03:01:17 -0800

    fix: update CSSRule interface to include string and number types for better flexibility

--- packages/css-js-gen/src/types.ts
+++ packages/css-js-gen/src/types.ts
@@ -16,7 +16,7 @@ export type CSSProperties = {
  * A CSS rule can contain properties and nested selectors
  */
 export interface CSSRule {
-	[key: string]: CSSRule | CSSProperties | CSSPropertiesOptions;
+	[key: string]: string | number | CSSRule | CSSProperties | CSSPropertiesOptions;
 }
 
 /**
