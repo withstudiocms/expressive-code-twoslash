b1ef908c3399b93ab7bad93c92617f2cd3f0e80f
Adam Matthiesen <amatthiesen@outlook.com>
2026-02-21 03:06:35 -0800

    feat: comment out shikiColorizedBrackets plugin configuration in ec.config.mjs

--- docs/ec.config.mjs
+++ docs/ec.config.mjs
@@ -1,10 +1,10 @@
 import { defineEcConfig } from "@astrojs/starlight/expressive-code";
 import ectwoslash from "expressive-code-twoslash";
-import shikiColorizedBrackets from "./plugins/shiki-color-brackets/index.js";
+// import shikiColorizedBrackets from "./plugins/shiki-color-brackets/index.js";
 
 export default defineEcConfig({
 	plugins: [ectwoslash()],
-	shiki: {
-		transformers: [shikiColorizedBrackets()],
-	},
+	// shiki: {
+	// 	transformers: [shikiColorizedBrackets()],
+	// },
 });
